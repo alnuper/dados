8c0ad58cef40089bc788898b09c0e9de6faebe54
alnuper <alnuper@yahoo.es>
2024-04-26 11:10:42 +0200

    Primera version del juego de dados

---
+++ app.js
@@ -0,0 +1,30 @@
+const dice1 = document.querySelector(".img1");
+const dice2 = document.querySelector(".img2");
+const resultado = document.querySelector("h1")
+
+const nombre = 'Alberto'
+
+
+
+
+
+let randomNumber1 = Math.floor(Math.random() * 6 + 1);
+let randomNumber2 = Math.floor(Math.random() * 6 + 1);
+
+dice1.setAttribute("src", "./images/dice" + randomNumber1 + ".png")
+//dice2.setAttribute("src", "./images/dice" + randomNumber2 + ".png")
+dice2.setAttribute("src", `./images/dice${randomNumber2}.png`)
+
+if (randomNumber1 > randomNumber2) {
+    resultado.textContent = "🥇 Player 1 Wins"
+} else if (randomNumber1 < randomNumber2) {
+    resultado.textContent = "Player 2 Wins 🥇"
+} else {
+    resultado.textContent = "Draw"    
+}
+
+console.log(randomNumber1);
+console.log(randomNumber2);
+
+console.log(`Hola, ${nombre}"`)
+
